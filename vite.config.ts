bd411761caad6a80570658837502465b5e1bf7b0
Ottoniel Castaneda Aguilar <56406635+OttonielCA@users.noreply.github.com>
2024-12-08 18:28:37 -0500

    Premier commit

---
+++ vite.config.ts
@@ -0,0 +1,61 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/Lab01_VueJs/',
+  plugins: [
+    vue(),
+    VitePWA({
+      // add this to cache all the imports
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      // add this to cache all the
+      // static assets in the public folder
+      includeAssets: [
+        "**/*",
+      ],
+      manifest: {
+        "theme_color": "#ff0000",
+        "background_color": "#ff0000",
+        "display": "standalone",
+        "scope": "/",
+        "start_url": "/",
+        "name": "Boutique Apple",
+        "short_name": "Apple Store",
+        "description": "Apple store available without network",
+        "icons": [
+          {
+            "src": "/icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/icons/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "/icons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "/icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+      }
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
