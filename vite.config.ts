2831f94dd13da1250c6fdf1a95b98bf8336853a4
Ottoniel Castaneda Aguilar <56406635+OttonielCA@users.noreply.github.com>
2024-12-08 19:39:55 -0500

    Ajout des images et correction des chemins d'accès

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      // add this to cache all the imports
       workbox: {
         globPatterns: ["**/*"],
       },
-      // add this to cache all the
-      // static assets in the public folder
       includeAssets: [
         "**/*",
       ],
@@ -22,29 +17,29 @@ export default defineConfig({
         "theme_color": "#ff0000",
         "background_color": "#ff0000",
         "display": "standalone",
-        "scope": "/",
-        "start_url": "/",
+        "scope": "/Lab01_VueJs/",
+        "start_url": "/Lab01_VueJs/",
         "name": "Boutique Apple",
         "short_name": "Apple Store",
         "description": "Apple store available without network",
         "icons": [
           {
-            "src": "/icons/icon-192x192.png",
+            "src": "/Lab01_VueJs/icons/icon-192x192.png",
             "sizes": "192x192",
             "type": "image/png"
           },
           {
-            "src": "/icons/icon-256x256.png",
+            "src": "/Lab01_VueJs/icons/icon-256x256.png",
             "sizes": "256x256",
             "type": "image/png"
           },
           {
-            "src": "/icons/icon-384x384.png",
+            "src": "/Lab01_VueJs/icons/icon-384x384.png",
             "sizes": "384x384",
             "type": "image/png"
           },
           {
-            "src": "/icons/icon-512x512.png",
+            "src": "/Lab01_VueJs/icons/icon-512x512.png",
             "sizes": "512x512",
             "type": "image/png"
           }
@@ -52,6 +47,7 @@ export default defineConfig({
       }
     }),
   ],
+  base: '/Lab01_VueJs/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
